6ceb385e15d15fb0b65ba87da66076997943f2bd
ashish <guptaashish7860@gmail.com>
2019-01-19 14:57:55 +0530

    Integrate android library BubbleSeekBar.

--- app/src/main/java/com/checkin/app/checkin/Shop/ShopInvoice/ShopInvoiceFeedbackAdapter.java
+++ app/src/main/java/com/checkin/app/checkin/Shop/ShopInvoice/ShopInvoiceFeedbackAdapter.java
@@ -2,14 +2,28 @@ package com.checkin.app.checkin.Shop.ShopInvoice;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class ShopInvoiceFeedbackAdapter extends RecyclerView.Adapter<ShopInvoiceFeedbackAdapter.ShopInvoiceFeedbackHolder>{
+import com.borjabravo.readmoretextview.ReadMoreTextView;
+import com.checkin.app.checkin.R;
+import com.xw.repo.BubbleSeekBar;
+
+import butterknife.BindView;
+
+public class ShopInvoiceFeedbackAdapter extends RecyclerView.Adapter<ShopInvoiceFeedbackAdapter.ShopInvoiceFeedbackHolder> {
+
     @NonNull
     @Override
     public ShopInvoiceFeedbackHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.child_shop_invoice_feedback, parent, false);
+        return new ShopInvoiceFeedbackHolder(view);
     }
 
     @Override
@@ -19,11 +33,43 @@ public class ShopInvoiceFeedbackAdapter extends RecyclerView.Adapter<ShopInvoice
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 3;
     }
 
-    class ShopInvoiceFeedbackHolder extends RecyclerView.ViewHolder{
-        public ShopInvoiceFeedbackHolder(View itemView) {
+    class ShopInvoiceFeedbackHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.tv_shop_invoice_feedback_customer_name)
+        TextView tvShopInvoiceFeedbackCustomerName;
+        @BindView(R.id.tv_shop_invoice_feedback_experience_title)
+        TextView tvShopInvoiceFeedbackExperienceTitle;
+        @BindView(R.id.tv_shop_invoice_feedback_experience)
+        ReadMoreTextView tvShopInvoiceFeedbackExperience;
+        @BindView(R.id.sb_shop_invoice_feedback_food_quality_rating)
+        BubbleSeekBar sbShopInvoiceFeedbackFoodQualityRating;
+        @BindView(R.id.sb_shop_invoice_feedback_ambience_rating)
+        BubbleSeekBar sbShopInvoiceFeedbackAmbienceRating;
+        @BindView(R.id.sb_shop_invoice_feedback_service_hospitality)
+        BubbleSeekBar sbShopInvoiceFeedbackServiceHospitality;
+        @BindView(R.id.rl_shop_invoice_feedback_rating)
+        RelativeLayout rlShopInvoiceFeedbackRating;
+        @BindView(R.id.incl_shop_invoice_detail_feedback_overall_rating)
+        RelativeLayout incl_shop_invoice_detail_feedback_overall_rating;
+        @BindView(R.id.ll_shop_invoice_detail_feedback_overall_rating)
+        LinearLayout llShopInvoiceDetailFeedbackOverallRating;
+        @BindView(R.id.v_shop_invoice_detail_feedback_image)
+        View vShopInvoiceDetailFeedbackImage;
+        @BindView(R.id.im_thumbnail_1)
+        ImageView imThumbnail1;
+        @BindView(R.id.im_thumbnail_2)
+        ImageView imThumbnail2;
+        @BindView(R.id.im_thumbnail_3)
+        ImageView imThumbnail3;
+        @BindView(R.id.tv_thumbnail_count)
+        TextView tvThumbnailCount;
+        @BindView(R.id.container_thumbnail_3)
+        FrameLayout containerThumbnail3;
+
+        ShopInvoiceFeedbackHolder(View itemView) {
             super(itemView);
         }
     }
--- app/src/main/java/com/checkin/app/checkin/Shop/ShopInvoice/ShopInvoiceFeedbackFragment.java
+++ app/src/main/java/com/checkin/app/checkin/Shop/ShopInvoice/ShopInvoiceFeedbackFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -36,6 +37,10 @@ public class ShopInvoiceFeedbackFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        ShopInvoiceFeedbackAdapter shopInvoiceFeedbackAdapter = new ShopInvoiceFeedbackAdapter();
+        rvShopInvoiceFeedback.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rvShopInvoiceFeedback.setAdapter(shopInvoiceFeedbackAdapter);
     }
 
     @Override
