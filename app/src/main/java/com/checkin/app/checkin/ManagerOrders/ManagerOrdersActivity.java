58f3e0855dfffa49a4c61dbf37e3609322c88a06
unknown <divasingh.92@gmail.com>
2019-01-23 23:12:50 +0530

    Manager orders layout partially done

---
+++ app/src/main/java/com/checkin/app/checkin/ManagerOrders/ManagerOrdersActivity.java
@@ -0,0 +1,15 @@
+package com.checkin.app.checkin.ManagerOrders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.checkin.app.checkin.R;
+
+public class ManagerOrdersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_orders);
+    }
+}
