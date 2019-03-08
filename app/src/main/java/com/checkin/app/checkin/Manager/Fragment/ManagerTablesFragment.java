4472d28a3298ce96ca6122c250664f569e1dbc8b
ashish <guptaashish7860@gmail.com>
2019-03-08 18:21:40 +0530

    Manager design and functionality of activated and non-activated screen has been completed.

--- app/src/main/java/com/checkin/app/checkin/Manager/Fragment/ManagerTablesFragment.java
+++ app/src/main/java/com/checkin/app/checkin/Manager/Fragment/ManagerTablesFragment.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.checkin.app.checkin.Data.Message.MessageModel;
 import com.checkin.app.checkin.Data.Message.MessageObjectModel;
@@ -15,6 +17,7 @@ import com.checkin.app.checkin.Data.Resource;
 import com.checkin.app.checkin.Manager.Adapter.ManagerWorkTableAdapter;
 import com.checkin.app.checkin.Manager.ManagerSessionActivity;
 import com.checkin.app.checkin.Manager.ManagerWorkViewModel;
+import com.checkin.app.checkin.Misc.BaseFragment;
 import com.checkin.app.checkin.Misc.BriefModel;
 import com.checkin.app.checkin.R;
 import com.checkin.app.checkin.Session.ActiveSession.Chat.SessionChatModel.CHAT_EVENT_TYPE;
@@ -22,14 +25,18 @@ import com.checkin.app.checkin.Session.Model.EventBriefModel;
 import com.checkin.app.checkin.Session.Model.RestaurantTableModel;
 import com.checkin.app.checkin.Utility.Utils;
 
+import java.util.List;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 import static com.checkin.app.checkin.Data.Message.MessageModel.MESSAGE_TYPE.MANAGER_SESSION_CHECKOUT_REQUEST;
@@ -39,16 +46,17 @@ import static com.checkin.app.checkin.Data.Message.MessageModel.MESSAGE_TYPE.MAN
 import static com.checkin.app.checkin.Data.Message.MessageModel.MESSAGE_TYPE.MANAGER_SESSION_NEW;
 import static com.checkin.app.checkin.Data.Message.MessageModel.MESSAGE_TYPE.MANAGER_SESSION_NEW_ORDER;
 
-public class ManagerTablesFragment extends Fragment implements ManagerWorkTableAdapter.ManagerTableInteraction {
-    private static final String TAG = ManagerTablesFragment.class.getSimpleName();
-
-    private Unbinder unbinder;
+public class ManagerTablesFragment extends BaseFragment implements ManagerWorkTableAdapter.ManagerTableInteraction {
 
     @BindView(R.id.rv_shop_manager_table)
     RecyclerView rvShopManagerTable;
+    @BindView(R.id.ll_no_orders)
+    LinearLayout llNoLiveOrders;
 
     private ManagerWorkTableAdapter mAdapter;
     private ManagerWorkViewModel mViewModel;
+
+    private List<RestaurantTableModel> mListData;
 
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
@@ -88,21 +96,32 @@ public class ManagerTablesFragment extends Fragment implements ManagerWorkTableA
     public ManagerTablesFragment() {
     }
 
+    @Override
+    protected int getRootLayout() {
+        return R.layout.fragment_shop_manager_table;
+    }
+
     public static ManagerTablesFragment newInstance() {
         return new ManagerTablesFragment();
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_shop_manager_table, container, false);
-        unbinder = ButterKnife.bind(this, view);
-        return view;
+    private void updateUi(List<RestaurantTableModel> data){
+        if(data.size() > 0){
+            mAdapter.setRestaurantTableList(data);
+            rvShopManagerTable.setVisibility(View.VISIBLE);
+            llNoLiveOrders.setVisibility(View.GONE);
+        }
+        else{
+            rvShopManagerTable.setVisibility(View.GONE);
+            llNoLiveOrders.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+
+        initRefreshScreen(R.id.sr_manager_table);
+
         mAdapter = new ManagerWorkTableAdapter(this);
         rvShopManagerTable.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
         rvShopManagerTable.setAdapter(mAdapter);
@@ -111,7 +130,14 @@ public class ManagerTablesFragment extends Fragment implements ManagerWorkTableA
         mViewModel.getActiveTables().observe(this, input -> {
             if (input == null) return;
             if (input.status == Resource.Status.SUCCESS && input.data != null) {
-                mAdapter.setRestaurantTableList(input.data);
+                mListData = input.data;
+                updateUi(mListData);
+                stopRefreshing();
+            } else if (input.status == Resource.Status.LOADING)
+                startRefreshing();
+            else {
+                stopRefreshing();
+                Utils.toast(requireContext(),input.message);
             }
         });
         mViewModel.getCheckoutData().observe(this, resource -> {
@@ -165,6 +191,7 @@ public class ManagerTablesFragment extends Fragment implements ManagerWorkTableA
                 MANAGER_SESSION_EVENT_CONCERN, MANAGER_SESSION_HOST_ASSIGNED
         };
         MessageUtils.registerLocalReceiver(requireContext(), mReceiver, types);
+        updateScreen();
     }
 
     @Override
@@ -174,9 +201,9 @@ public class ManagerTablesFragment extends Fragment implements ManagerWorkTableA
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        unbinder.unbind();
+    protected void updateScreen() {
+        super.updateScreen();
+        mViewModel.updateResults();
     }
 
     @Override
