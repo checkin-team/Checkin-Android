413f442d1253cbfda02f8b345af7bc8cba78c62d
ashish <guptaashish7860@gmail.com>
2019-03-12 20:57:30 +0530

    Manager Bill Generate is completed.

--- app/src/main/java/com/checkin/app/checkin/Manager/ManagerSessionActivity.java
+++ app/src/main/java/com/checkin/app/checkin/Manager/ManagerSessionActivity.java
@@ -309,4 +309,14 @@ public class ManagerSessionActivity extends AppCompatActivity implements Manager
                 break;
         }
     }
+
+    @Override
+    public void onGenerateBillClick() {
+        if (mSessionData == null) return;
+        Intent intent = new Intent(this, ManagerSessionInvoiceActivity.class);
+        intent.putExtra(ManagerSessionInvoiceActivity.TABLE_NAME, mSessionData.getTable())
+                .putExtra(ManagerSessionInvoiceActivity.KEY_SESSION, mViewModel.getSessionPk())
+                .putExtra(ManagerSessionInvoiceActivity.IS_REQUESTED_CHECKOUT, mSessionData.isRequestedCheckout());
+        startActivity(intent);
+    }
 }
