1510076b714bb77b9d4a9697555a1713fc4c7efc
ashish <guptaashish7860@gmail.com>
2019-03-27 18:56:32 +0530

    Added condtion to remove OutOfMemory Error in SelectCropImageActivity.

--- app/src/main/java/com/checkin/app/checkin/Misc/SelectCropImageActivity.java
+++ app/src/main/java/com/checkin/app/checkin/Misc/SelectCropImageActivity.java
@@ -1,18 +1,10 @@
 package com.checkin.app.checkin.Misc;
 
 import android.app.Activity;
-import android.content.ContentUris;
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.Matrix;
-import android.media.ExifInterface;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.util.Log;
 
@@ -25,54 +17,44 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SelectCropImageActivity extends AppCompatActivity {
-    private static final String TAG = SelectCropImageActivity.class.getSimpleName();
-
     public static final String KEY_IMAGE = "select_crop.image";
     public static final String KEY_CROP_ASPECT_RATIO = "aspect_ratio";
-
+    public static final String KEY_FILE_SIZE_IN_MB = "file_size_in_mb";
     public static final int RC_CROP_IMAGE = 1000;
-    private static final long MAX_FILZE_SIZE_IN_BYTES = 4 * 1024 * 1024;
+    private static final String TAG = SelectCropImageActivity.class.getSimpleName();
     private static final int RC_PICK_IMAGE = 100;
-    private File mRectangleFile;
 
+    private static final long ONE_BYTES_IN_MB = 1024 * 1024;
+    @BindView(R.id.crop_view)
     CropView cropView;
+
+    private File mRectangleFile;
+    private long maxFileSizeInBytes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_crop_image);
+        ButterKnife.bind(this);
 
-        requestImage();
-
+        long fileSizeInMB = getIntent().getLongExtra(KEY_FILE_SIZE_IN_MB, 0);
+        maxFileSizeInBytes = fileSizeInMB * ONE_BYTES_IN_MB;
         float viewportRatio = getIntent().getFloatExtra(KEY_CROP_ASPECT_RATIO, 0.81f);
 
-        mRectangleFile = new File(getCacheDir(), "cropped.png");
-        mRectangleFile.delete();
-
-        cropView = findViewById(R.id.crop_view);
         cropView.setViewportRatio(viewportRatio);
 
-        findViewById(R.id.btn_image_crop_done).setOnClickListener(v -> {
-            Bitmap bitmap = cropView.crop();
-            if (bitmap == null) {
-                Log.e(TAG, "Cropped bitmap is null!");
-                return;
-            }
-            try (FileOutputStream out = new FileOutputStream(mRectangleFile)) {
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        init();
+    }
 
-            long cropFileSizeInByte = mRectangleFile.length();
-            if (cropFileSizeInByte <= MAX_FILZE_SIZE_IN_BYTES)
-                setResult(Activity.RESULT_OK, getImageIntent());
-            else
-                Utils.toast(this,"File size is larger than 4MB.");
-            finish();
-        });
+    private void init() {
+        requestImage();
+        mRectangleFile = new File(getCacheDir(), "cropped.png");
+        mRectangleFile.delete();
     }
 
     private void requestImage() {
@@ -80,6 +62,30 @@ public class SelectCropImageActivity extends AppCompatActivity {
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), RC_PICK_IMAGE);
+    }
+
+    @OnClick(R.id.btn_image_crop_done)
+    public void doneCropImage() {
+        Bitmap bitmap = cropView.crop();
+        if (bitmap == null) {
+            Log.e(TAG, "Cropped bitmap is null!");
+            return;
+        }
+        try (FileOutputStream out = new FileOutputStream(mRectangleFile)) {
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        long cropFileSizeInByte = mRectangleFile.length();
+
+        if (maxFileSizeInBytes == 0 || cropFileSizeInByte <= maxFileSizeInBytes) {
+            setResult(Activity.RESULT_OK, getImageIntent());
+            finish();
+        } else {
+            Utils.toast(this, "File size is larger than " + maxFileSizeInBytes / ONE_BYTES_IN_MB + "MB");
+            init();
+        }
     }
 
     private Intent getImageIntent() {
@@ -98,9 +104,13 @@ public class SelectCropImageActivity extends AppCompatActivity {
         if (requestCode == RC_PICK_IMAGE) {
             Uri uri = data.getData();
             try {
+                /*Setting null value to setImageBitmap to remove OOM Error.*/
+                if (cropView.getImageBitmap() != null) {
+                    cropView.setImageBitmap(null);
+                }
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                String imageAbsolutePath = Utils.getPath(this,uri);
-                Bitmap mFinalBitmap = Utils.modifyOrientation(bitmap,imageAbsolutePath);;
+                String imageAbsolutePath = Utils.getPath(this, uri);
+                Bitmap mFinalBitmap = Utils.modifyOrientation(bitmap, imageAbsolutePath);
                 cropView.setImageBitmap(mFinalBitmap);
             } catch (IOException e) {
                 e.printStackTrace();
