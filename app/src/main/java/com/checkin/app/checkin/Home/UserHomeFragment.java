776fd27c4f2128918facb3d4c443d1d93b7ff6bd
Divya <divasingh.92@gmail.com>
2019-08-05 18:54:06 +0530

    Banner and profile icon changes done

--- app/src/main/java/com/checkin/app/checkin/Home/UserHomeFragment.java
+++ app/src/main/java/com/checkin/app/checkin/Home/UserHomeFragment.java
@@ -1,19 +1,30 @@
 package com.checkin.app.checkin.Home;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.checkin.app.checkin.Misc.BaseFragment;
 import com.checkin.app.checkin.R;
 import com.checkin.app.checkin.session.activesession.ActiveSessionMemberAdapter;
+import com.rd.PageIndicatorView;
+import com.rd.animation.type.AnimationType;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import butterknife.BindView;
 import butterknife.OnClick;
 
@@ -21,7 +32,12 @@ public class UserHomeFragment extends BaseFragment {
 
     @BindView(R.id.rv_user_private_recent_shops)
     RecyclerView rvTrendingRestaurants;
+    @BindView(R.id.indicator_home_banner)
+    PageIndicatorView indicatorView;
+    @BindView(R.id.vp_home_banner)
+    ViewPager vpBanner;
     private UserHomeTrendingRestaurantAdapter mTrendingRestAdapter;
+    private BannerPagerAdapter mPagerAdapter;
 
     public UserHomeFragment() {
     }
@@ -49,6 +65,13 @@ public class UserHomeFragment extends BaseFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ((HomeActivity) Objects.requireNonNull(getActivity())).enableDisableSwipeRefresh(true);
+
+        mPagerAdapter = new BannerPagerAdapter(requireActivity());
+        vpBanner.setAdapter(mPagerAdapter);
+        indicatorView.setViewPager(vpBanner);
+        indicatorView.setAnimationType(AnimationType.FILL);
+        indicatorView.setClickListener(position -> vpBanner.setCurrentItem(position));
+
         rvTrendingRestaurants.setLayoutManager(new LinearLayoutManager(requireActivity(), RecyclerView.HORIZONTAL, false));
         mTrendingRestAdapter = new UserHomeTrendingRestaurantAdapter();
         rvTrendingRestaurants.setAdapter(mTrendingRestAdapter);
@@ -63,9 +86,66 @@ public class UserHomeFragment extends BaseFragment {
             }
         });
 
+        vpBanner.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                enableDisableSwipeRefresh(state == ViewPager.SCROLL_STATE_IDLE);
+            }
+        });
+
     }
 
     public void enableDisableSwipeRefresh(boolean enable) {
             ((HomeActivity) Objects.requireNonNull(getActivity())).enableDisableSwipeRefresh(enable);
     }
+
+    class BannerPagerAdapter extends PagerAdapter {
+
+        int[] mResources = {R.drawable.first_banner, R.drawable.second_banner, R.drawable.third_banner};
+
+        Context mContext;
+        LayoutInflater mLayoutInflater;
+
+        public BannerPagerAdapter(Context context) {
+            mContext = context;
+            mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return mResources.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return object == view;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View itemView = mLayoutInflater.inflate(R.layout.item_home_banner, container, false);
+            ImageView imageView = (ImageView) itemView.findViewById(R.id.im_home_banner);
+            imageView.setImageResource(mResources[position]);
+            container.addView(itemView);
+
+            return itemView;
+
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            container.removeView(((View) object));
+        }
+
+    }
+
+
 }
