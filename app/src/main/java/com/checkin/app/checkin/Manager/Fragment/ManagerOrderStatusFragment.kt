de1ab300feb0d5b111488019f6419660d14b1e29
pallav12 <rajeevpallav606@gmail.com>
2019-12-22 04:55:29 +0530

    Completed Manager Status UI

---
+++ app/src/main/java/com/checkin/app/checkin/Manager/Fragment/ManagerOrderStatusFragment.kt
@@ -0,0 +1,130 @@
+package com.checkin.app.checkin.Manager.Fragment
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+
+import com.checkin.app.checkin.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [ManagerOrderStatusFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [ManagerOrderStatusFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ManagerOrderStatusFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        var v=inflater.inflate(R.layout.fragment_manager_order_status, container, false)
+        var imageView=v.findViewById<ImageView>(R.id.im_manager_status_booking_arrow)
+        var view=v.findViewById(R.id.include_manager_status_booking_detail)as View
+        var j=0
+        imageView.setOnClickListener {
+            if (j == 0) {
+                view.visibility = View.GONE
+                j = 1
+            } else {
+                view.visibility = View.VISIBLE
+                j = 0
+            }
+        }
+
+            var imageView2=v.findViewById<ImageView>(R.id.im_fragment_manager_status_details_arrow)
+            var view2=v.findViewById(R.id.includ_fragment_order_status_bill)as View
+            var i=0
+            imageView2.setOnClickListener{
+                if(i==0){
+                    view2.visibility=View.GONE
+                    i=1
+                }
+                else{
+                    view2.visibility=View.VISIBLE
+                    i=0
+                }
+
+
+
+        }
+
+
+        return v
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ManagerOrderStatusFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() =
+                ManagerOrderStatusFragment().apply {
+
+                }
+    }
+}
