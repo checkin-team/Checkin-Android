e4be9c94a22306c11b49483aeb2c8dade7381d60
Jeevan Surendran <jvns67@gmail.com>
2020-01-21 01:21:19 +0530

    App version was made horizontal center

--- app/src/main/java/com/checkin/app/checkin/home/activities/AboutUsActivity.kt
+++ app/src/main/java/com/checkin/app/checkin/home/activities/AboutUsActivity.kt
@@ -1,7 +1,6 @@
 package com.checkin.app.checkin.home.activities
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.content.pm.PackageManager
