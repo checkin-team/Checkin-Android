8a92083a0bdf873a77253371bd3bccdab1ffd4e5
Shivansh Saini <shivanshs9@gmail.com>
2020-10-17 03:33:49 +0530

    chore: Version bump -> 1.2.4
    Signed-off-by: Shivansh Saini <shivanshs9@gmail.com>

--- app/src/main/java/com/checkin/app/checkin/utility/Constants.kt
+++ app/src/main/java/com/checkin/app/checkin/utility/Constants.kt
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit.MILLISECONDS
 import java.util.concurrent.TimeUnit.MINUTES
 
 object Constants {
-    const val API_VERSION = "1.3.1"
+    const val API_VERSION = "1.3.3"
     const val API_PROTOCOL = "https://"
     val API_HOST
         get() = if (IS_RELEASE_BUILD()) "api.check-in.in" else "dev.api.check-in.in"
